8e8514aa94f2dde90b8d99cd605241fd89f2193e
yuweizzz <yuwei764969238@gmail.com>
2025-04-24 15:00:50 +0800

    build script

---
+++ brotli_dictionary_generator/build.sh
@@ -0,0 +1,9 @@
+cd libdivsufsort/
+cmake . -DBUILD_SHARED_LIBS=OFF
+make
+cd ..
+g++ -c brotli/research/deorummolae.cc -I esaxx/
+g++ -c brotli/research/durchschlag.cc
+g++ -c brotli/research/sieve.cc
+g++ -c brotli/research/dictionary_generator.cc
+g++ -o brotli_dictionary_generator *.o -L libdivsufsort/lib/ -ldivsufsort -static
