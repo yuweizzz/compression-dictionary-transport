b2a74661b41539f6a72051aed6ada34c685c0945
yuweizzz <yuwei764969238@gmail.com>
2025-05-08 17:12:47 +0800

    fix workflow

--- brotli_dictionary_generator/build.sh
+++ brotli_dictionary_generator/build.sh
@@ -3,7 +3,7 @@ cmake . -DBUILD_SHARED_LIBS=OFF
 make
 cd ..
 g++ -c brotli/research/deorummolae.cc -I esaxx/
-g++ -c brotli/research/durchschlag.cc
+g++ -c brotli/research/durchschlag.cc -I libdivsufsort/include
 g++ -c brotli/research/sieve.cc
 g++ -c brotli/research/dictionary_generator.cc
 g++ -o brotli_dictionary_generator *.o -L libdivsufsort/lib/ -ldivsufsort -static
